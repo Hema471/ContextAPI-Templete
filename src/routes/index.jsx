010660648cf69289d1c07bb05cb8128fc700075a
Hema471 <143406609+Hema471@users.noreply.github.com>
2024-04-10 18:50:01 +0200

    * route added

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,10 +3,13 @@ import App from "../App";
 
 const router = createBrowserRouter([
   {
+    path: "*",
+    element: <h1 className="font-bold text-3xl">Page Not Found</h1>,
+  },
+  {
     path: "/",
     element: <App />,
   },
-
 ]);
 
 export default router;
